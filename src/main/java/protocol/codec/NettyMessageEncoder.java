5f4ac05cb78f83ce8f6df621166f646c4a638adc
snailxr <snailxr@163.com>
2015-12-02 10:32:01 +0800

    add

---
+++ src/main/java/protocol/codec/NettyMessageEncoder.java
@@ -0,0 +1,57 @@
+package protocol.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import protocol.message.NettyMessage;
+
+public class NettyMessageEncoder extends MessageToMessageEncoder<NettyMessage> {
+	MarshallingEncoder marshllingEncoder;
+
+	public NettyMessageEncoder() throws IOException {
+		this.marshllingEncoder = new MarshallingEncoder();
+	}
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, NettyMessage msg, List<Object> out) throws Exception {
+		if (msg == null || msg.getHeader() == null) {
+			throw new Exception(" the encode message is null");
+		}
+
+		ByteBuf sendBuf = Unpooled.buffer();
+		sendBuf.writeInt(msg.getHeader().getCrcCode());
+		sendBuf.writeInt(msg.getHeader().getLength());
+		sendBuf.writeLong(msg.getHeader().getSessionID());
+		sendBuf.writeByte(msg.getHeader().getType());
+		sendBuf.writeByte(msg.getHeader().getPriority());
+		sendBuf.writeByte(msg.getHeader().getAttachment().size());
+		String key = null;
+		byte[] keyArray = null;
+		Object value = null;
+		for (Map.Entry<String, Object> param : msg.getHeader().getAttachment().entrySet()) {
+			key = param.getKey();
+			keyArray = key.getBytes("UTF-8");
+			sendBuf.writeInt(keyArray.length);
+			sendBuf.writeBytes(keyArray);
+			value = param.getValue();
+			marshllingEncoder.encode(value, sendBuf);
+		}
+		key = null;
+		keyArray = null;
+		value = null;
+		if (msg.getBody() != null) {
+			marshllingEncoder.encode(msg.getBody(), sendBuf);
+		} else {
+			sendBuf.writeInt(0);
+			sendBuf.setIndex(4, sendBuf.readableBytes());
+		}
+
+	}
+
+}
